7d9c48b2bab8b81250cbbc31a317cf0216d03180
Cella <gerardo_cella@yahoo.fr>
2017-11-28 08:58:56 +0100

    jour 1

---
+++ serie1/page2.php
@@ -0,0 +1,3 @@
+<?php 
+	echo '<p>page2.php</p>';
+?>
---
+++ serie2/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title>Examen - Page 1</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/style.css"/>
+</head>
+<body>
+	<nav class="navbar navbar-inverse" id="navbar">
+	  <div class="container-fluid">
+	    <div class="navbar-header">
+	      <a class="navbar-brand" href="Page1.php">Le site d'actualité</a>
+	    </div>
+	    <ul class="nav navbar-nav">
+	      <li><a href="Page1.php">Régions</a></li>
+	      <li><a href="Page2.php">Foot</a></li>
+	    </ul>
+	  </div>
+	</nav>
